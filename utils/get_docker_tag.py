dfa2e501d3f3a00a8f4e5f8a365bf6a49f36e05d
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2019-11-13 14:24:45 -0800

    get_docker_tag cli now functional

--- utils/get_docker_tag.py
+++ utils/get_docker_tag.py
@@ -1,16 +1,28 @@
 import nbformat
 import argparse
 
-#nbformat.reads
 
 def get_args_parser():
     parser = argparse.ArgumentParser(
       description="Read Docker Container TAG that is required by the Notebook")
     parser.add_argument("--path", required=True)
-    return pareser
+    parser.add_argument("--test", required=False, action='store_true')
+    return parser
 
+
+def get_container_uri_from_nb_dict(nb_obj):
+    if "env" not in nb_obj.metadata:
+        raise ValueError("There is no 'env' key in the Notebook's metadata")
+    return nb_obj.metadata.env.container
 
 if __name__ == "__main__":
     args = get_args_parser().parse_args()
-    nb_obj = nbformat.reads(unicode(args.apth), nbformat.NO_CONVERT)
-    print(nb_obj)
+    with open(args.path,"r") as nb_file: 
+        nb_obj = nbformat.read(nb_file, nbformat.NO_CONVERT)
+        ## Adding metadat for testing
+        if args.test:
+            nb_obj.metadata["env"] = {
+                "container": "gcr.io/deeplearning-platform-release/pytorch-cpu"
+            }
+        ##
+        print(get_container_uri_from_nb_dict(nb_obj))
