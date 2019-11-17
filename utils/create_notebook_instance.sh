205d005f224b303bd906006453299c41c4a3d2d0
Mike Cheng <mikehcheng@google.com>
2019-11-16 18:59:41 -0800

    copy notebook when finished

--- utils/create_notebook_instance.sh
+++ utils/create_notebook_instance.sh
@@ -13,6 +13,20 @@
 #   second argument is the path to the notebook to source and upload
 #   additional arguments are passed into the instance creation command
 
+function get_proxy_url() {
+  local INSTANCE_NAME=$1
+  gcloud compute instances describe ${INSTANCE_NAME} --format='value[](metadata.items.proxy-url)'
+}
+
+function wait_for_proxy_url() {
+  local INSTANCE_NAME=$1
+  while [[ -z "$(get_proxy_url ${INSTANCE_NAME})" ]]; do
+    echo "Notebook instance not ready, waiting."
+    sleep 15
+  done
+  echo "Notebook URL: $(get_proxy_url ${INSTANCE_NAME})"
+}
+
 INSTANCE_NAME=$1
 NOTEBOOK=$2
 shift 2
@@ -20,11 +34,21 @@ shift 2
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
 NOTEBOOK_CONTAINER="$(python ${SCRIPT_DIR}/get_docker_tag.py --path=${NOTEBOOK})"
 
+echo "Creating instance."
 gcloud compute instances create "${INSTANCE_NAME}" \
   --image-family=common-container \
   --image-project=deeplearning-platform-release \
   --maintenance-policy=TERMINATE \
-  --boot-disk-size=200GB \
+  --boot-disk-size=50GB \
   --scopes=https://www.googleapis.com/auth/cloud-platform \
   --metadata="proxy-mode=project_editors,install-nvidia-driver=True,container=${NOTEBOOK_CONTAINER}" \
   "$@"
+
+wait_for_proxy_url "${INSTANCE_NAME}"
+
+echo "Waiting for finalization of startup."
+sleep 15
+
+echo "Copying notebook."
+gcloud compute scp "${NOTEBOOK}" "${INSTANCE_NAME}:/home/jupyter/$(basename ${NOTEBOOK})"
+
